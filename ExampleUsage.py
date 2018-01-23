32c8a3fac965c6012293704f7f2f27f107a5d401
tpoff <tpoff@vols.utk.edu>
2018-01-22 23:57:44 -0600

    Moved conversion code to it's own script and made sample usage. Also added a test image curtesy of github.

---
+++ ExampleUsage.py
@@ -0,0 +1,2 @@
+from Image_To_Excel_Spreadsheet import Convert_Image_To_Excel_Spreadsheet
+Convert_Image_To_Excel_Spreadsheet("octobiwan.jpg")
--- Image_To_Excel_Spreadsheet.py
+++ Image_To_Excel_Spreadsheet.py
@@ -1,59 +1,37 @@
 from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import PatternFill
-
-
+import openpyxl
 from PIL import Image, ImageDraw, ImageFont
 
-wb = load_workbook('./test.xlsx')
-print(wb.get_sheet_names())
 
-#get sheet object.
-sheet1 = wb.get_sheet_by_name('Sheet1')
-#print(sheet3['A1'].value)
-#cell=sheet3['A1']
+def Convert_Image_To_Excel_Spreadsheet(image_path, resolution=.3, output_file="results.xlsx"):
+    wb = openpyxl.Workbook()
+    sheet = wb.get_sheet_by_name("Sheet")
+    im = Image.open(image_path)
+    width, height = im.size
+    rgb_im = im.convert('RGB')
+    for x in range(0, int(width * resolution)):
+        column_indicies = [get_column_letter((x * 3) + 1), get_column_letter((x * 3) + 2),
+                           get_column_letter((x * 3) + 3)]
+        sheet.column_dimensions[column_indicies[0]].width = 10 / 9
+        sheet.column_dimensions[column_indicies[1]].width = 10 / 9
+        sheet.column_dimensions[column_indicies[2]].width = 10 / 9
+        for row in range(1, int(height * resolution)):
+            rgb_array = rgb_im.getpixel((int(x / resolution), int(row / resolution)))
+            for i in range(3):
+                colors = [0, 0, 0]
+                colors[i] = rgb_array[i]
+                col = get_column_letter((x * 3) + i + 1)
 
-#sheet3.max_row
-#sheet3.max_column
+                cell = sheet[col + str(row)]
 
+                cell.value = rgb_array[i]
+                color_string = "".join([str(hex(i))[2:].upper().rjust(2, "0") for i in colors])
+                cell.fill = PatternFill(fill_type="solid", start_color='FF' + color_string,
+                                        end_color='FF' + color_string)
 
-#sheet3['B2'] = "YO MAMA"
-
-
-
-im = Image.open("image.jpg")
-width,height = im.size
-rgb_im = im.convert('RGB')
-
-resolution = 1
-print(width)
-print(int(width*resolution))
-print()
-print(height)
-print(int(height*resolution))
-for x in range(0, int(width*resolution)):
-    print("column: "+str(x))
-    column_indicies = [get_column_letter((x*3)+1),get_column_letter((x*3)+2),get_column_letter((x*3)+3)]
-    sheet1.column_dimensions[column_indicies[0]].width = 10/9
-    sheet1.column_dimensions[column_indicies[1]].width = 10/9
-    sheet1.column_dimensions[column_indicies[2]].width = 10/9
-    for row in range(1, int(height*resolution)):
-        rgb_array = rgb_im.getpixel((int(x/resolution),int(row/resolution)))
-        for i in range(3):
-            colors =[0,0,0]
-            colors[i] = rgb_array[i]
-            col = get_column_letter((x * 3) + i+1)
-
-            cell = sheet1[col + str(row)]
-
-            cell.value = rgb_array[i]
-            color_string = "".join([str(hex(i))[2:].upper().rjust(2, "0") for i in colors])
-            cell.fill=PatternFill(fill_type="solid", start_color='FF' + color_string, end_color='FF' + color_string)
-
-
-
-
-wb.save('./test.xlsx')
+    wb.save(output_file)
 
 
 
